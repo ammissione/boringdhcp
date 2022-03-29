dbeb80dc579bb7f2402871974c2d7f334338d077
Lorenzo Durastante <durastante.lorenzo02@gmail.com>
2022-03-29 08:49:38 +0200

    added link to GitHub's repo

--- src/boringdhcp/BoringDHCP.java
+++ src/boringdhcp/BoringDHCP.java
@@ -5,8 +5,8 @@ public class BoringDHCP {
     public static void main(String[] args) {
         DHCPAddressPool.loadRange("192.168.1.1", "192.168.1.10");
         DHCPAddressPool.loadRange("10.0.0.1", "10.0.0.20");
-        System.out.println("Initializing BoringDHCP v1.0 by Lorenzo Durastante\n");
-        System.out.println("OpenSource on Github <https://github.com/ammissione/boringdhcp>");
+        System.out.println("Initializing BoringDHCP v1.0 by Lorenzo Durastante");
+        System.out.println("OpenSource on Github <https://github.com/ammissione/boringdhcp>\n");
         DHCPAddressPool.showAvailableAddresses();
         DHCPManager.registerClient("admin");
         DHCPManager.assignIP("admin", DHCPManager.selectIP());
